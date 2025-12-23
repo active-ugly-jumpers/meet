b595816112e57aac15d3c3a99982e2f46589eed2
p <pasha.sem@tutamail.com>
2025-12-23 14:07:30 +0100

    Added PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import * as atatus from 'atatus-spa';
-atatus.config('4caaab2ccca9407388dc1ba9bc5a9bf4').install();
+// import * as atatus from 'atatus-spa';
+// atatus.config('4caaab2ccca9407388dc1ba9bc5a9bf4').install();
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+serviceWorkerRegistration.register();
